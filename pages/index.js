880b358cae83724b7e2fa996f5d54299efdbfb02
Mykyta Medvediev <arlekin8881@gmail.com>
2022-09-18 12:39:15 -0700

    Header on Index page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
+import Link from "next/link";
+import { Container, Box, Heading } from "@chakra-ui/react";
 
-export default function Index() {
+export default function Index({ children, router }) {
   return (
-    <div>
-      Index
-      <br />
-      <Link href="/works">Works page</Link>
-      <br />
-      <Link href="/about">About page</Link>
-    </div>
-  )
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a full-stack developer!
+      </Box>
+
+      <Box display={{ md: "flex" }}>
+        <Box flexGrow={1}>
+			<Heading as="h2" variant="page-title">Mykyta Medvediev</Heading>
+		</Box>
+      </Box>
+      <div>
+        Index
+      </div>
+    </Container>
+  );
 }
