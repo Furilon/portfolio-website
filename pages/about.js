e794a4bac0f1ee81c6384e8aaf88481e077bd809
Mykyta Medvediev <arlekin8881@gmail.com>
2022-09-18 11:47:05 -0700

    Added 3 pages

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export default function About() {
+    return (
+      <div>
+        About
+
+        <Link href="/">Back to Index</Link>
+        <Link href="/works">Works page</Link>
+      </div>
+    )
+  }
+  
---
+++ pages/works.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export default function Works() {
+    return (
+      <div>
+        Works
+
+        <Link href="/">Back to Index</Link>
+        <Link href="/about">About page</Link>
+      </div>
+    )
+  }
+  
