142e1268e4fe68c8bbeb1462705b30e806a7176c
Mykyta Medvediev <arlekin8881@gmail.com>
2022-09-19 13:35:48 -0700

    Renamed the main layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import "../styles/globals.css";
-import Layout from "../components/layout";
+import MainLayout from "../components/layouts/main";
 import { ChakraProvider } from "@chakra-ui/react";
+import { AnimatePresence } from "framer-motion";
 
 function MyApp({ Component, pageProps, router }) {
-  	return (
-		<ChakraProvider>
-			<Layout router={router}>
-				<Component {...pageProps} key={router.route} />
-			</Layout>
-		</ChakraProvider>
-  	)
+  return (
+    <ChakraProvider>
+      <MainLayout router={router}>
+        <AnimatePresence exitBeforeEnter initial={true}>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </MainLayout>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
