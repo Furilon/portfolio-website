3afc6258ae2127d6ff1fa63b56b1d2430459a4ca
Mykyta Medvediev <arlekin8881@gmail.com>
2022-10-22 18:03:50 -0700

    Added meta tags

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -6,10 +6,29 @@ export default function MainLayout({ children, router }) {
   return (
     <Box as="main" pb={8}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Mykyta Medvediev - Homepage</title>
+        <meta charSet="UTF-8" />
+        <meta name="application-name" content="Portfolio Website" />
+        <meta name="author" content="Mykyta Medvediev" />
+        <meta
+          name="description"
+          content="Mykyta Medvediev's Portfolio Website."
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/Favicon.jpg" type="image/x-icon" />
+
+        {/* tags for when the website is shared */}
+        <meta
+          property="og:description"
+          content="Mykyta Medvediev's Portfolio Website."
+        />
+        <meta
+          property="og:title"
+          content="Mykyta Medvediev's Portfolio Website"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/Logo_with_background.png" />
       </Head>
-      <Navbar path={router.asPath} />
+      <Navbar />
       <Container maxW="container.md" pt={14}>
         {children}
       </Container>
