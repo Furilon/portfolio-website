aaca92f9d4c158e001925c85bcdb87ee665a33b6
Mykyta Medvediev <arlekin8881@gmail.com>
2022-10-25 15:48:00 -0700

    Removed underlining on experience item heading

--- components/experience.js
+++ components/experience.js
@@ -3,7 +3,7 @@ import { Flex, Text, Heading, UnorderedList, ListItem } from "@chakra-ui/react";
 export default function Experience({ name, date, pointsArr }) {
   return (
     <Flex direction="column" justify="center" align="center" mt="20px">
-      <Heading as="h4" fontSize="1.2rem" textDecoration="underline">
+      <Heading as="h4" fontSize="1.2rem" fontStyle={}>
         {name}
       </Heading>
       <Flex justify="center" align="flex-start" mt="10px">
