ee9c53d246d20f34229768d1f9fc03e317ef3891
Mykyta Medvediev <arlekin8881@gmail.com>
2022-10-30 14:15:33 -0700

    Added membership-board problems and solutions

--- pages/project/[projectName].js
+++ pages/project/[projectName].js
@@ -50,9 +50,11 @@ const PROJECT_PROBLEMS = {
     "This is my first big vanilla js project. The goal was to implement the app while making the code as modular as possible (using factory funcitons and IIFEs). I faced two problems with that. The first one was about adhering to DRY principle, not leaving anything in the global space, and making everything modular. The second problem was understanding and using closures properly: I needed to pass manupulate the state in a complicated manner, passing functions and variables, which are scoped within those functions, around. And since I don't have a lot of control over the functions that manipulate DOM, I struggled with passing the state of DOM elements around.",
   "cv-application":
     "This is my first React project. The goal was to learn manipulating state: to get the input from user on the 'input' side and display it on the 'resume' side. The main problem I faced was the one of setting up and updating a complex state.",
-  "shopping-cart": "Shopping Cart",
+  "shopping-cart":
+    "This is the first React project where I used react-router-dom. The main problem was to manage state between the pages. When a user clicks on 'Add to cart' button, the flower goes into the cart, where he could see the subtotal for that particular flower (if there's more than one of the same kind), to add more or remove flowers of the same kind, and to see the total for all flowers in the cart.",
   "blog-user-frontend": "Blog",
-  "membership-club": "Membership Club",
+  "membership-club":
+    "This is my first Express app that required authentication. The problems were to connect functionality of database, authentication, and showing the correct page to the user based on the route.",
 };
 
 const PROJECT_SOLUTIONS = {
@@ -62,9 +64,11 @@ const PROJECT_SOLUTIONS = {
     "I couldn't solve the first issue immediately. Because I was very early on my JS journey, I didn't know what first-order functions were and how scoping worked in details, and I didn't even know that I didn't know it, and because of that I couldn't make the code DRY immediately. So, I completed the project the way I could (making it barely functional), read a book from You Don't Know Javascript series about scoping and closures, went back and refactored the code to adhere to those principles. To not leave anything in the global scope and make the code modular, I learned about the factory funciton pattern and the module pattern (IIFE), and I connected them using an interface module. The issue of closures was also solved after reading the aforementioned book. I started encapsulating the state of DOM elements in functions and then pass those functions where I needed.",
   "cv-application":
     "Using functional components, I set up three state variables: for personal info, for experience, and one for education. For the application, the number of fields in the personal state was constant, so it's simply a matter of capturing the input and putting it into the correct object field. Experience and Education sections were harder because there, the user is supposed to be able to add more of those sections or delete them. It means that the state for each of those sections needs to be a list of objects, so I had to learn how to access the needed object and change fields in it without changing the rest of the fields and the whole state. For that, I learned to use object destructuring and methods that don't change the original state variable (.push() vs .concat(), for example). Lastly, I combined these three funcitons into one onChange() function and picked which method to invoke based on the clicked button's id.",
-  "shopping-cart": "Shopping Cart",
+  "shopping-cart":
+    "To solve that problem, I put all of the flowers and their prices into a 'global' state. When the user clicks on the 'Add to cart' button, the function fetches that flower from the state and passes it into the cart component. The cart component keeps the state of passed flowers, checks whether the flower with this ID is already present. Based on that, it does either of two thigns: if the flower is indeed present, it finds that flower in its state and adds its price to itself; if it doesn't exist, then it simply adds it to the state and displays it in the cart. When the user clicks '+' and '-' buttons, the functions determined what flower in the state user wants to add or remove and simply adds the price or removes it. If the price after removal equals to 0, the cart component removes the flower from the state.",
   "blog-user-frontend": "Blog",
-  "membership-club": "Membership Club",
+  "membership-club":
+    "To solve that problem, I used Model-View-Controller (MVC) pattern, where the app is thought of as a collection of controller functions, which are invoked based on what route user's request specified, that fetch requested data form the database (I used MongoDB), filter and sanitize it, pass it into views (HTML templates, I used EJS templating language), which are sent to the client. I also used Passport-local authentication strategy with session, and I restricted some routes based on whether the user is logged in or not. Additionally, the user isn't able to see some information on the message board unless he is signed in.",
 };
 
 export async function getStaticPaths() {
