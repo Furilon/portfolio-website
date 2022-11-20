4406b0e9417dfe824ce40c34e4b7e4fc5064e587
Mykyta Medvediev <arlekin8881@gmail.com>
2022-11-19 18:20:31 -0800

    Updated links for vennbury university

--- pages/project/[projectName].js
+++ pages/project/[projectName].js
@@ -2,7 +2,7 @@ import Project from "../../components/project";
 import PageLayout from "../../components/layouts/page";
 
 const LIVE_LINKS = {
-  pandora: "https://theprojectpandora.com",
+  vennbury: "https://vennbury.com",
   "to-do-app": "https://furilon.github.io/to-do-app",
   "cv-application": "https://furilon.github.io/cv-application",
   "shopping-cart": "https://furilon.github.io/shopping-cart",
@@ -11,7 +11,7 @@ const LIVE_LINKS = {
 };
 
 const DESCRIPTIONS = {
-  pandora:
+  vennbury:
     "Mastery-based and project-focused learning platform. Become an expert by building projects.",
   "to-do-app":
     "This is my first vanilla JS project with separation of concerns using OOP and other abstract patterns, like factory functions and module pattern.",
@@ -26,7 +26,7 @@ const DESCRIPTIONS = {
 };
 
 const IMAGES = {
-  pandora: "/pandora.png",
+  vennbury: "/vennbury.png",
   "to-do-app": "/to-do-app.png",
   "cv-application": "/cv-application.png",
   "shopping-cart": "/shopping-cart.png",
@@ -35,7 +35,7 @@ const IMAGES = {
 };
 
 const PROJECT_NAMES = {
-  pandora: "Project Pandora",
+  vennbury: "Vennbury",
   "to-do-app": "To-do Application",
   "cv-application": "CV Generator",
   "shopping-cart": "Shopping Cart",
@@ -44,7 +44,7 @@ const PROJECT_NAMES = {
 };
 
 const PROJECT_PROBLEMS = {
-  pandora:
+  vennbury:
     "This is my first serious production-ready full-stack project, and there were (and still are) many problems along the way. The first issue we faced is building a React application (SPA) and connecting it to Express server. I quickly realized that we want to have many pages, and that if we only have an SPA, then a page won't load unless it's a homepage, which makes link sharing impossible and hinders SEO. The second problem was team management: we have a team of two, and I'm the 'visionary' in it, so I have to make all of the strategic decisions alongside project management. The third problem was about making lesson page creation scalable: since we are planning to have a big number of lessons, it's a big hassle to create a page for each lesson.",
   "to-do-app":
     "This is my first big vanilla js project. The goal was to implement the app while making the code as modular as possible (using factory funcitons and IIFEs). I faced two problems with that. The first one was about adhering to DRY principle, not leaving anything in the global space, and making everything modular. The second problem was understanding and using closures properly: I needed to pass manupulate the state in a complicated manner, passing functions and variables, which are scoped within those functions, around. And since I don't have a lot of control over the functions that manipulate DOM, I struggled with passing the state of DOM elements around.",
@@ -59,7 +59,7 @@ const PROJECT_PROBLEMS = {
 };
 
 const PROJECT_SOLUTIONS = {
-  pandora:
+  vennbury:
     "To solve the issue of connecting the server with the client, we initially tried to set up a hybrid solution that hydrates the React page on the server and sends it to the client as a ready HTML page (Server-Side Rendering), but it was overly complicated from the architectural standpoint, full of hydration bugs, and very unoptimized. So, I made a decision to switch to NextJS, that solved all backend problems, provided much more pleasant developer experience, and lets us easily choose between SSR and CSR. To solve the team management issue, I set up a Trello app with three boards: to-do, in-process, and done. For each task, we create a card and assign labels that indicate urgency/importance, come up with weekly due dates, and delegate tasks. It allowed us to visualize what tasks are important and prioritize them, to track who's responsible for what, and spread what's needed to do among the team. The last problem is the most interesting one. We want to write lessons in Markdown, so we needed to agree on a convention for the structure of those lessons, after which we designed an algorithm to recursively search through the 'lessons' folder, parse the text, and put it into our components.",
   "to-do-app":
     "I couldn't solve the first issue immediately. Because I was very early on my JS journey, I didn't know what first-order functions were and how scoping worked in details, and I didn't even know that I didn't know it, and because of that I couldn't make the code DRY immediately. So, I completed the project the way I could (making it barely functional), read a book from You Don't Know Javascript series about scoping and closures, went back and refactored the code to adhere to those principles. To not leave anything in the global scope and make the code modular, I learned about the factory funciton pattern and the module pattern (IIFE), and I connected them using an interface module. The issue of closures was also solved after reading the aforementioned book. I started encapsulating the state of DOM elements in functions and then pass those functions where I needed.",
@@ -76,7 +76,7 @@ const PROJECT_SOLUTIONS = {
 export async function getStaticPaths() {
   return {
     paths: [
-      { params: { projectName: "pandora" } },
+      { params: { projectName: "vennbury" } },
       { params: { projectName: "to-do-app" } },
       { params: { projectName: "cv-application" } },
       { params: { projectName: "shopping-cart" } },
