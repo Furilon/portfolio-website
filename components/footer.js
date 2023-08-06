7607a76d9304daed2a78118d308f77b398aca10c
Mykyta Medvediev <arlekin8881@gmail.com>
2023-08-06 11:49:23 -0700

    Changed the year

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Flex, Text } from "@chakra-ui/react";
 export default function Footer() {
   return (
     <Flex justify="center" align="center" mt="auto">
-      <Text textColor="#cdcdcd">Mykyta Medvediev, 2022</Text>
+      <Text textColor="#cdcdcd">Mykyta Medvediev, 2023</Text>
     </Flex>
   );
 }
