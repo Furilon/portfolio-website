ca48cfe1c6e5800829154611ffdb7bf7b05ca3d3
Mykyta Medvediev <arlekin8881@gmail.com>
2023-10-26 22:34:48 -0700

    Made blog work in a similar way and added mock data

---
+++ data/articles/4 basic myths.md
@@ -0,0 +1,41 @@
+---
+tags: type/note
+#theme/ - build cluster 
+#index/ - define entry point
+# 
+#type/ - note characteristics
+#source/ - where note comes from
+#target/ - where note will go to
+# 
+#chart/ - from #type/chart
+#role/ - from #type/person
+#visual/ - from #type/visual
+#
+aliases:
+#
+created: 2023-02-15, 18:45
+modified: 2023-02-15, 18:45
+---
+
+# 4 basic myths
+
+> [!NOTE]
+> Good myths provide answers to these three questions:
+> 1. What is the nature (meaning, the significance) of current state?
+> 2. To what (desirable) end should it be moving?
+> 3. What are the processes by which the current state might be transformed into the desired state?
+> 
+> There are 4 basic myths:
+> 1. That describe a current or pre-existent stable state (paradise, tyranny);
+> 2. That describe appearance of something anomalous, threatening and promising into this initial state;
+> 3. That describe the dissolution of the pre-existent stable state into chaos, as a consequence of the anomalous or unexpected occurence;
+> 4. That describe the regeneration of stability (paradise regained or tyranny regenerated), from the chaotic mixture of dissolute previous experience and anomalous information.
+
+
+---
+## Tags
+#theme/myth #theme/mythology 
+
+---
+## References
+- [[Maps of Meaning]] 16
---
+++ pages/library/blog/index.js
@@ -0,0 +1,58 @@
+import matter from "gray-matter";
+
+import {
+  Box,
+  Flex,
+  Text,
+  Heading,
+  ListItem,
+  UnorderedList,
+  Link,
+} from "@chakra-ui/react";
+import PageLayout from "../../../components/layouts/page";
+
+export async function getStaticProps() {
+  const path = require("path");
+  const fs = require("fs");
+
+  let linksMap = {};
+
+  function throughDirectory(directory) {
+    fs.readdirSync(directory).forEach((file) => {
+      const fileName = path.basename(file).split(".")[0];
+      const link = createLink(fileName);
+
+      linksMap[fileName] = link;
+    });
+  }
+
+  function createLink(fileName) {
+    const lowerCaseFileName = fileName.toLowerCase();
+    const formattedFileName = lowerCaseFileName.replace(/ /g, "-");
+    const link = `/library/blog/${formattedFileName}`;
+
+    return link;
+  }
+
+  throughDirectory("data/articles");
+
+  const serializedLinksMap = JSON.stringify(linksMap);
+
+  return { props: { serializedLinksMap } };
+}
+
+export default function Blog({ serializedLinksMap }) {
+  const linksMap = JSON.parse(serializedLinksMap);
+
+  return (
+    <PageLayout title="all blogs">
+      <UnorderedList>
+        {Object.entries(linksMap).map(([key, value]) => (
+          <ListItem key={key}>
+            <Link href={value}>{key}</Link>
+          </ListItem>
+        ))}
+      </UnorderedList>
+    </PageLayout>
+  );
+}
